dcbc7017b8361194a00f74ba4382e55ab266f5fc
jamarob <janmartinrobert@gmail.com>
2022-03-23 10:46:13 +0100

    Add login endpoint

--- api/login.mjs
+++ api/login.mjs
@@ -1,5 +1,38 @@
-const handler = (request, response) => {
-  response.status(501).json('NOT IMPLEMENTED')
+import connectToMongodb from '../lib/db/connect-to-mongodb.mjs'
+import User from '../lib/model/User.mjs'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const handler = async (request, response) => {
+  const { method } = request
+
+  if (method !== 'POST') {
+    return response.status(405).json('Method not allowed')
+  }
+
+  const { name, password } = request.body
+
+  if (!(name && password)) {
+    return response.status(400).json('Missing credential')
+  }
+
+  await connectToMongodb()
+
+  const foundUser = await User.findOne({ name })
+
+  if (!foundUser) {
+    return response.status(401).json('Unauthorized')
+  }
+
+  const isMatch = await bcrypt.compare(password, foundUser.password)
+
+  if (!isMatch) {
+    return response.status(401).json('Unauthorized')
+  }
+
+  const token = jwt.sign({ sub: foundUser._id }, 'super-secret')
+
+  response.status(200).json(token)
 }
 
 export default handler
