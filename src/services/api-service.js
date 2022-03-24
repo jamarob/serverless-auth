19b81beaf3b240d9bf135136e65297f3c28f322a
jamarob <janmartinrobert@gmail.com>
2022-03-24 09:04:49 +0100

    Assert 200 responses

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,24 +1,38 @@
+const assertOk = response =>
+  response.then(async res => {
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error(await res.json())
+  })
+
 export const postUsernameAndPassword = credentials =>
-  fetch('/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(credentials),
-  }).then(res => res.json())
+  assertOk(
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+  )
 
 export const postGitHubAuthorizationCode = code =>
-  fetch('/api/github-login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ code }),
-  }).then(res => res.json())
+  assertOk(
+    fetch('/api/github-login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code }),
+    })
+  )
 
 export const getProfile = token =>
-  fetch('/api/profile', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }).then(res => res.json())
+  assertOk(
+    fetch('/api/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  )
